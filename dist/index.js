f488f57d6bd9a26dc45ffc3f0aa3a5fc9f54aab8
Matilde <matilde.moretti95@gmail.com>
2026-07-26 15:00:59 +0200

    Add functionality to create new tasks

--- dist/index.js
+++ dist/index.js
@@ -28,9 +28,22 @@ function displayTodoList() {
 }
 var Commands;
 (function (Commands) {
+    // create new tasks
+    Commands["Add"] = "Add New Task";
     Commands["Toggle"] = "Show/Hide Completed";
     Commands["Quit"] = "Quit";
 })(Commands || (Commands = {}));
+function promptAdd() {
+    console.clear();
+    inquirer
+        .prompt({ type: "input", name: "add", message: "Enter task:" })
+        .then((answers) => {
+        if (answers["add"] !== "") {
+            collection.addTodo(answers["add"]);
+        }
+        promptUser();
+    });
+}
 function promptUser() {
     console.clear();
     displayTodoList();
@@ -46,6 +59,9 @@ function promptUser() {
             case Commands.Toggle:
                 showCompleted = !showCompleted;
                 promptUser();
+                break;
+            case Commands.Add:
+                promptAdd();
                 break;
         }
     });
--- src/index.ts
+++ src/index.ts
@@ -35,8 +35,22 @@ function displayTodoList(): void {
 }
 
 enum Commands {
+  // create new tasks
+  Add = "Add New Task",
   Toggle = "Show/Hide Completed",
   Quit = "Quit",
+}
+
+function promptAdd(): void {
+  console.clear();
+  inquirer
+    .prompt({ type: "input", name: "add", message: "Enter task:" })
+    .then((answers) => {
+      if (answers["add"] !== "") {
+        collection.addTodo(answers["add"]);
+      }
+      promptUser();
+    });
 }
 
 function promptUser(): void {
@@ -54,6 +68,9 @@ function promptUser(): void {
         case Commands.Toggle:
           showCompleted = !showCompleted;
           promptUser();
+          break;
+        case Commands.Add:
+          promptAdd();
           break;
       }
     });
